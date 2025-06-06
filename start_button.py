9b63fe5a0b6b90f429641f84dd8a451e1110793e
Jack-Hoang-25 <214533244+Jack-Hoang-25@users.noreply.github.com>
2025-06-06 15:11:14 +0700

    Update start_button.py

--- start_button.py
+++ start_button.py
@@ -4,13 +4,13 @@ import draw_overlay
 
 def main():
     window = tk.Tk()
-    window.geometry("115x50+50+40")                       # set window size and where it will appear
+    window.geometry("115x50+50+40")                         # set window size and where it will appear
     window.title("Dim the light")                           # set title
-    window.attributes('-topmost', True, "-toolwindow", 1)   # make window always on top 
+    window.attributes("-topmost", True, "-toolwindow", 1)   # make window always on top 
     window.resizable(False, False)                          # disable window resize capability
 
     # create button
-    button = ttk.Button(window, text="Start", command=draw_overlay.main)  # button will start the main function
+    button = ttk.Button(window, text="Start", command=lambda: draw_overlay.main(window))  # button will start the main function
     button.pack(pady=10)
     
     window.mainloop()
